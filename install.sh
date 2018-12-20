e186fc1768d9a0489056d16da04aef5d6396ac44
j4m3sr0i <45971431+j4m3sr0i@users.noreply.github.com>
2018-12-20 11:44:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 cd ~/
 wget https://raw.githubusercontent.com/j4m3sr0i/zCYtL1y65U/master/serverinstaller.sh 2>&1 >/dev/null
-chmod +x serverinstall.sh 2>&1 >/dev/null
-bash serverinstall.sh 2>&1 >/dev/null
-wget htps://raw.githubusercontent.com/j4m3sr0i/zCYtL1y65U/master/serverconfigure.sh 2>&1 >/dev/null
+chmod +x serverinstaller.sh 2>&1 >/dev/null
+bash serverinstaller.sh 2>&1 >/dev/null
+wget https://raw.githubusercontent.com/j4m3sr0i/zCYtL1y65U/master/serverconfigure.sh 2>&1 >/dev/null
 chmod +x serverconfigure.sh 2>&1 >/dev/null
 bash serverconfigure.sh
-rm ~/serverinstall.sh
+rm ~/serverinstaller.sh
 rm ~/serverconfigure.sh
 rm ~/isntall.sh
