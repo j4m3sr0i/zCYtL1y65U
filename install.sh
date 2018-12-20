0a2c896922905fe1645288dbda73e359c55236f5
j4m3sr0i <45971431+j4m3sr0i@users.noreply.github.com>
2018-12-20 08:56:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 cd ~/
-curl --request GET --header 'PRIVATE-TOKEN: MykKrUmaQZts1F9QoF6W' 'https://gitlab.com/api/v4/projects/9957454/repository/files/serverinstall.sh/raw?ref=master' 2>&1 >/dev/null
+wget https://raw.githubusercontent.com/j4m3sr0i/zCYtL1y65U/master/serverinstaller.sh 2>&1 >/dev/null
 chmod +x serverinstall.sh 2>&1 >/dev/null
 bash serverinstall.sh 2>&1 >/dev/null
-curl --request GET --header 'PRIVATE-TOKEN: MykKrUmaQZts1F9QoF6W' 'https://gitlab.com/api/v4/projects/9957454/repository/files/serverconfigure.sh/raw?ref=master' 2>&1 >/dev/null
+wget htps://raw.githubusercontent.com/j4m3sr0i/zCYtL1y65U/master/serverconfigure.sh 2>&1 >/dev/null
 chmod +x serverconfigure.sh 2>&1 >/dev/null
 bash serverconfigure.sh
 rm ~/serverinstall.sh
